66e9c7d87943a15db75f7edb86d301c30104d080
aleeeexcool <ramzhev.sasha@outlook.com>
2023-03-17 13:19:37 +0200

    add tests for withdraw()

--- test/DAO-test.js
+++ test/DAO-test.js
@@ -29,7 +29,7 @@ describe("DAO", function () {
             voteEndTime = Math.floor(Date.now() / 1000) + 600;
         });
 
-        it("Shoud be have a deployer and machine address", async function() {
+        it('Shoud be have a deployer and machine address', async function() {
             const { machine, dao } = await loadFixture(deploy);
 
             let machineAddr = await dao.VendingMachineAddress();
@@ -48,7 +48,7 @@ describe("DAO", function () {
             expect(newBalance.sub(initialBalance)).to.equal(depositAmount);
         });
 
-        it("should revert if called before the end of voting period", async function () {
+        it('should revert if called before the end of voting period', async function () {
             const { dao } = await loadFixture(deploy);
 
             await expect(dao.countVote()).to.be.revertedWith("Vote not yet ended.");
@@ -64,7 +64,7 @@ describe("DAO", function () {
             expect(dao.connect(account1).Deposit({ value: depositAmount2 })).be.revertedWith('1 Ether balance has been reached');
         });
 
-        it("Should revert on double vote", async function () {
+        it('Should revert on double vote', async function () {
             const { dao, account1 } = await loadFixture(deploy);
 
             const yes = 0;
@@ -73,49 +73,71 @@ describe("DAO", function () {
             await expect(dao.connect(account1).vote(yes)).to.be.reverted;
         });
 
-        it("Should revert if the balance has not enough funds", async function () {
+        it('Should revert if the balance has not enough funds', async function () {
             const { dao, account1 } = await loadFixture(deploy);
 
-            //testing
+            await dao.connect(account1).Deposit({ value: ethers.utils.parseEther('1.0') });
+
+            await expect(dao.connect(account1).withdraw(ethers.utils.parseEther('2.0'))).to.be.revertedWith('Insufficient funds on the balance!');
+        });
+
+        it('Should withdraw successfully when the balance is sufficient', async function () {
+            const { dao, account1 } = await loadFixture(deploy);
+
+            await dao.connect(account1).Deposit({ value: ethers.utils.parseEther('1.0') });
+
+            await expect(dao.connect(account1).withdraw(ethers.utils.parseEther('1.0')));
+        });
+
+        it('should update the DAO balance after a withdrawal', async function () {
+            const { dao, account1 } = await loadFixture(deploy);
+
+            await dao.connect(account1).Deposit({ value: ethers.utils.parseEther('1.0') });
+            const initialDAObalance = await ethers.provider.getBalance(dao.address);
+
+            await dao.connect(account1).withdraw(ethers.utils.parseEther('0.5'));
+            const finalDAObalance = await ethers.provider.getBalance(dao.address);
+
+            expect(finalDAObalance).to.be.equal(initialDAObalance.sub(ethers.utils.parseEther('0.5')));
         });
         
         //need to add test giveRightToVote()
 
-        it("Should vote and return cookieBalance", async function () {
-            const { dao, account1, account2, account3 } = await loadFixture(deploy);
+        // it('Should vote and return cookieBalance', async function () {
+        //     const { dao, account1, account2, account3 } = await loadFixture(deploy);
     
-            const yes = 0;
-            const no = 1;
+        //     const yes = 0;
+        //     const no = 1;
     
-            let amountPayable = {value: ethers.utils.parseEther("0.5")};
+        //     let amountPayable = {value: ethers.utils.parseEther("0.5")};
     
-            await dao.connect(account1).Deposit(amountPayable);
-            await dao.connect(account2).Deposit(amountPayable);
+        //     await dao.connect(account1).Deposit(amountPayable);
+        //     await dao.connect(account2).Deposit(amountPayable);
     
-            await dao.connect(account1).vote(yes);
-            await dao.connect(account2).vote(yes);
-            await dao.connect(account3).vote(no);
+        //     await dao.connect(account1).vote(yes);
+        //     await dao.connect(account2).vote(yes);
+        //     await dao.connect(account3).vote(no);
     
-            await ethers.provider.send("evm_increaseTime", [(24 * 60 * 60) + 60]);
-            await network.provider.send("evm_mine");
+        //     await ethers.provider.send("evm_increaseTime", [(24 * 60 * 60) + 60]);
+        //     await network.provider.send("evm_mine");
     
-            await dao.countVote();
-            let decision = await dao.decision();
+        //     await dao.countVote();
+        //     let decision = await dao.decision();
     
-            expect(decision).to.equal(0);
+        //     expect(decision).to.equal(0);
     
-            await dao.EndVote();
+        //     await dao.EndVote();
 
-            // let cookieBalance = await dao.checkCookieBalance();
-            // await dao.connect(account1).checkCookieBalance();
+        //     let cookieBalance = await dao.checkCookieBalance();
+        //     await dao.connect(account1).checkCookieBalance();
 
-            // expect(cookieBalance).to.equal(1);
+        //     expect(cookieBalance).to.equal(1);
     
-            // let cookieBalance = await dao.checkCookieBalance();
+        //     let cookieBalance = await dao.checkCookieBalance();
     
-            // expect(cookieBalance).to.equal(1);
+        //     expect(cookieBalance).to.equal(1);
     
-            // console.log("cookie balance: ", cookieBalance);
-        });
+        //     console.log("cookie balance: ", cookieBalance);
+        // });
     });
 });
