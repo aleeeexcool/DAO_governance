43631aca36ef30697f1bce68d4926acc7c0faaac
aleeeexcool <ramzhev.sasha@outlook.com>
2023-03-21 18:57:43 +0200

    deploy.js done

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,24 @@
 const hre = require("hardhat")
-const ethers = hre.ethers
-
 
 async function main() {
-  const [signer, user1, user2] = await ethers.getSigners()
+    const VendingMachine = await hre.ethers.getContractFactory('VendingMachine')
+    const machine = await VendingMachine.deploy()
 
-  const Erc = await ethers.getContractFactory('TANShop', signer)
-  const erc = await Erc.deploy()
-  await erc.deployed()
-  console.log(erc.address)
-  console.log(await erc.token())
+    await machine.deployed()
 
-  const Governance = await ethers.getContractFactory('Governance', signer, user1, user2)
-  const governance = await Governance.deploy()
-  await erc.deployed()
+    const proposals = ['buy_cookie', 'no_cookie']
+
+    const DAO = await ethers.getContractFactory('DAO')
+    const dao = await DAO.deploy()
+
+    await dao.deployed(machine.address, 86400, proposals)
+
+    console.log("DAO contract deployed to: ", dao.address);
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
